5b8cbafd34ca6fdd20f087e78a5911cd3853602c
romeogoon <olarn.iot@gmail.com>
2020-02-17 16:56:44 +0700

    Create node-red-container.sh

---
+++ node-red-container.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Run Container
+docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/root/data --name node-red nodered/node-red:latest-12
+
+# Copy node-red modules to /root/data directory 
+docker exec -d -it node-red /bin/sh -c "cd /usr/src/ && cp -R /usr/src/* /root/data"
+
+# Move node-red modules from /data to /node-red
+mv /root/Documents/Projects/node-red/data/* /root/Documents/Projects/node-red/
+
+# Remove Container
+docker rm -f node-red
+
+# Run Container again
+docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/data ~/Documents/Projects/node-red/node-red:/usr/src/node-red/ --name node-red nodered/node-red:latest-12
+
+# Enjoy!
