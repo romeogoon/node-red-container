73a91b86106b2666489f2c6dae7f2249969978da
romeogoon <olarn.iot@gmail.com>
2020-02-18 11:16:54 +0700

    Update node-red-container.sh

--- node-red-container.sh
+++ node-red-container.sh
@@ -7,25 +7,29 @@
 #
 ################################################################################
 
-# Run Container
-docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/root/data --name node-red nodered/node-red:latest-12
+container_name=node-red
+# Check container status
+if sudo docker ps -a --format '{{.Names}}' | grep -Eq "^${container_name}\$"; then
+	echo 'This container already exists'
+else
+	# Run Container
+	docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/root/data --name node-red nodered/node-red:latest-12
 
-sleep 5
+	sleep 5
 
-# Copy node-red modules to /root/data directory 
-docker exec -d -it node-red /bin/sh -c "cd /usr/src/ && cp -R /usr/src/* /root/data"
+	# Copy node-red modules to /root/data directory 
+	docker exec -d -it node-red /bin/sh -c "cd /usr/src/ && cp -R /usr/src/* /root/data"
 
-sleep 5
+	sleep 5
 
-# Move node-red modules from /data to /node-red
-mv /root/Documents/Projects/node-red/data/* /root/Documents/Projects/node-red/
+	# Move node-red modules from /data to /node-red
+	mv /root/Documents/Projects/node-red/data/* /root/Documents/Projects/node-red/
 
-# Remove Container
-docker rm -f node-red
+	# Remove Container
+	docker rm -f node-red
 
-sleep 3
+	sleep 3
 
-# Run Container again
-docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/data -v ~/Documents/Projects/node-red/node-red:/usr/src/node-red/ --name node-red nodered/node-red:latest-12
-
-# Enjoy!
+	# Run Container again
+	docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/data -v ~/Documents/Projects/node-red/node-red:/usr/src/node-red/ --name node-red nodered/node-red:latest-12
+fi
