a5ecf4e91a62fd42b9674780b178d67548ad7bc1
romeogoon <olarn.iot@gmail.com>
2020-02-18 09:58:10 +0700

    Update node-red-container.sh

--- node-red-container.sh
+++ node-red-container.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 ################################################################################
-# Author:   Olarn Sukasem
+# Author: Olarn Sukasem
 #
-# Description:
-#   Files management in Node-RED container
+# Description: Files management in Node-RED container
 #
 ################################################################################
 
@@ -16,13 +15,17 @@ sleep 5
 # Copy node-red modules to /root/data directory 
 docker exec -d -it node-red /bin/sh -c "cd /usr/src/ && cp -R /usr/src/* /root/data"
 
+sleep 5
+
 # Move node-red modules from /data to /node-red
 mv /root/Documents/Projects/node-red/data/* /root/Documents/Projects/node-red/
 
 # Remove Container
 docker rm -f node-red
 
+sleep 3
+
 # Run Container again
-docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/data ~/Documents/Projects/node-red/node-red:/usr/src/node-red/ --name node-red nodered/node-red:latest-12
+docker run -d --rm -u root -it -p 1880:1880 -v ~/Documents/Projects/node-red/data:/data -v ~/Documents/Projects/node-red/node-red:/usr/src/node-red/ --name node-red nodered/node-red:latest-12
 
 # Enjoy!
