5bcda755ae7c1125c4ba0b089810d1c9e79571d8
romeogoon <olarn.iot@gmail.com>
2020-02-18 11:16:58 +0700

    Update node-red-hash-pw.sh

--- node-red-hash-pw.sh
+++ node-red-hash-pw.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+docker exec -d -it node-red /bin/sh -c "mkdir opt && chmod 777 opt"
+docker cp node-red-hash-pw.sh node-red:/usr/src/node-red/opt
+
 node -e "console.log(require('bcryptjs').hashSync(process.argv[1], 8));" olarn
