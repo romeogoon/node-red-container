89a018dd7fdfa7be1476bccbc03e232a1e5c96a4
romeogoon <olarn.iot@gmail.com>
2020-02-19 10:19:18 +0700

    Update node-red-hash-pw.sh

--- node-red-hash-pw.sh
+++ node-red-hash-pw.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+################################################################################
+# Author: Olarn Sukasem
+#
+# Description: Generate a hash
+#
+################################################################################
+
+# Create and change permission
 docker exec -d -it node-red /bin/sh -c "mkdir /usr/src/node-red/opt && chmod 777 /usr/src/node-red/opt"
+
+# Copy hash-pw.sh to container
 docker cp /root/Node-RED-Container-master/hash-pw.sh node-red:/usr/src/node-red/opt
+
+# Change permission and execute hash-pw.sh
 docker exec -it node-red /bin/sh -c "chmod 777 /usr/src/node-red/opt/hash-pw.sh && /usr/src/node-red/opt/hash-pw.sh"
