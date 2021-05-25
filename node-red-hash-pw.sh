7f0b87043524247aa7e36493d5dfc130d244a163
romeogoon <olarn.iot@gmail.com>
2021-05-26 03:07:57 +0700

    Update node-red-hash-pw.sh

--- node-red-hash-pw.sh
+++ node-red-hash-pw.sh
@@ -9,13 +9,13 @@
 
 # Login to root then cd to directory "/home/rock/Documents/Projects/node-red"
 MYPWD=${PWD}
-echo "${MYPWD}"
 
 # Create and change permission
-docker exec -d -it node-red /bin/sh -c "mkdir /usr/src/node-red/opt && chmod 777 /usr/src/node-red/opt"
+docker exec -d -it node-red /bin/sh -c "mkdir /usr/src/node-red/opt && chmod 755 /usr/src/node-red/opt"
 
 # Copy hash-pw.sh to container
 docker cp "${MYPWD}"/hash-pw.sh node-red:/usr/src/node-red/opt
 
 # Change permission and execute hash-pw.sh
-docker exec -it node-red /bin/sh -c "chmod 777 /usr/src/node-red/opt/hash-pw.sh && /usr/src/node-red/opt/hash-pw.sh"
+result=$(docker exec -it node-red /bin/sh -c "chmod 755 /usr/src/node-red/opt/hash-pw.sh && /usr/src/node-red/opt/hash-pw.sh "$1"")
+echo "${result}";
