1bde51e6850ef7cd3cdf52817abad5dd3f0adf90
romeogoon <olarn.iot@gmail.com>
2021-07-14 00:31:20 +0700

    Create download.sh

---
+++ download.sh
@@ -0,0 +1,5 @@
+wget https://raw.githubusercontent.com/romeogoon/node-red-container/master/node-red-container.sh
+
+wget https://raw.githubusercontent.com/romeogoon/node-red-container/master/node-red-hash-pw.sh
+
+wget https://raw.githubusercontent.com/romeogoon/node-red-container/master/hash-pw.sh
